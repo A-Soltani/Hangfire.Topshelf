5c1fb0893f98f89b3a1b19d4376847c7621bf23e
Ali Soltani <soltani1384@gmail.com>
2020-08-10 18:33:57 +0430

    fist commit

---
+++ Hangfire.Topshelf.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangfire.Topshelf.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
